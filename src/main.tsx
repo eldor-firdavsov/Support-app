e44a26de63b6c88d054ffd8e68d14ab762d617ad
Eldor <firdavsove1@gmail.com>
2026-07-03 15:41:59 +0500

    Integrate Telegram Web App SDK

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './index.css'
 
+// Initialize Telegram Mini App
+const initTelegram = () => {
+  const twa = (window as any).Telegram?.WebApp;
+  if (twa) {
+    twa.ready();
+    twa.expand();
+  }
+};
+
+initTelegram();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
